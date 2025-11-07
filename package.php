a2e5630f998bb7c30afdf7c944bd24d7beef5c90
Peter Hebert <peter@peterhebert.com>
2025-11-07 16:53:13 -0500

    fix autoloader

--- package.php
+++ package.php
@@ -9,8 +9,16 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-// Load the command class
-require_once __DIR__ . '/vendor/autoload.php';
+// Autoload the command class
+$autoloader = __DIR__ . '/vendor/autoload.php';
+if ( file_exists( $autoloader ) ) {
+	require_once $autoloader;
+}
+
+// Manually load the command class if autoloader isn't available
+if ( ! class_exists( 'PeterHebert\WPComposerSync\Composer_Sync_Command' ) ) {
+	require_once __DIR__ . '/src/Composer_Sync_Command.php';
+}
 
 // Register the command
 WP_CLI::add_command( 'composer sync', 'PeterHebert\WPComposerSync\Composer_Sync_Command' );
